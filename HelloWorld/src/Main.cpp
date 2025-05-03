e802c81a3e9e208599323841ed555c47d1923411
Simon Chen <simonc8192@gmail.com>
2025-05-02 21:06:48 -0700

    added level enum in log class

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -3,35 +3,36 @@
 class Log
 {
 public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
+	enum Level
+	{
+		LevelError, LevelWarning, LevelInfo
+	};
 
 private:
-	int m_LogLevel = LogLevelInfo;
+	Level m_LogLevel = LevelInfo;
 
 public:
-	void SetLevel(int level)
+	void SetLevel(Level level)
 	{
 		m_LogLevel = level;
 	}
 
 	void Error(const char* message)
 	{
-		if (m_LogLevel >= LogLevelError)
+		if (m_LogLevel >= LevelError)
 			std::cout << "[ERROR]: " << message << std::endl;
 	}
 
 	void Warn(const char* message)
 	{
-		if (m_LogLevel >= LogLevelWarning)
+		if (m_LogLevel >= LevelWarning)
 		std::cout << "[WARNING]: " << message << std::endl;
 	}
 	
 
 	void Info(const char* message)
 	{
-		if (m_LogLevel >= LogLevelInfo)
+		if (m_LogLevel >= LevelInfo)
 			std::cout << "[INFO]: " << message << std::endl;
 	}
 };
@@ -39,8 +40,10 @@ public:
 int main()
 {
 	Log log;
-	log.SetLevel(log.LogLevelWarning);
+	log.SetLevel(Log::LevelError);
 	log.Warn("Hello");
+	log.Error("Hello");
+	log.Info("Hello");
 
 	std::cin.get();
 }
