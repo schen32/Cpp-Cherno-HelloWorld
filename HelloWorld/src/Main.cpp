6a7ec60bccdedd3a362bf3e10b8567a638380902
Simon Chen <simonc8192@gmail.com>
2025-05-02 20:33:11 -0700

    basic log class

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -1,25 +1,46 @@
 #include <iostream>
 
-#define LOG(x) std::cout << x << std::endl
-
-class Player
+class Log
 {
 public:
-	int x, y;
-	int speed;
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
 
-	void Move(int xa, int ya)
+private:
+	int m_LogLevel = LogLevelInfo;
+
+public:
+	void SetLevel(int level)
 	{
-		x += xa * speed;
-		y += ya * speed;
+		m_LogLevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+		std::cout << "[WARNING]: " << message << std::endl;
+	}
+	
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
 	}
 };
 
 int main()
 {
-	Player player;
-	player.x = 5;
-	player.Move(-1, 1);
-	
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Warn("Hello");
+
 	std::cin.get();
 }
