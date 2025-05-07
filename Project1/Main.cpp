066661fcf3da85ef6fd5821d35a9bde84960a4c4
Simon Chen <simonc8192@gmail.com>
2025-05-07 01:42:50 -0700

    DynamicArray using templates

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -5,20 +5,53 @@ void pp(const int& i)
 	std::cout << &i << " " << i << " " << sizeof(i) << std::endl;
 }
 
+template <typename T>
+class DynamicArray
+{
+	T* m_arr;
+	size_t m_size;
+
+public:
+	DynamicArray(size_t size)
+		: m_arr(new T[size]), m_size(size)
+	{
+		std::cout << "Array constructor" << std::endl;
+	}
+
+	~DynamicArray()
+	{
+		delete[] m_arr;
+		std::cout << "Array deconstructor" << std::endl;
+	}
+
+	T get(size_t index) const
+	{
+		return m_arr[index];
+	}
+
+	void set(size_t index, T val)
+	{
+		m_arr[index] = val;
+	}
+
+	void print() const
+	{
+		for (size_t i = 0; i < m_size; i++)
+		{
+			std::cout << i << " " << m_arr[i] << std::endl;
+		}
+	}
+};
+
+
 int main()
 {
-	int arr[10] = {};
-	int* harr = new int[10];
+	DynamicArray<int> myArray(10);
 
-	for (size_t i = 0; i < 10; i++)
-	{
-		pp(arr[i]);
-	}
-	for (size_t i = 0; i < 10; i++)
-	{
-		pp(harr[i]);
-	}
+	myArray.set(4, 7);
+	myArray.set(2, 134);
 
-	delete[] harr;
+	myArray.print();
+
 	return 0;
 }
