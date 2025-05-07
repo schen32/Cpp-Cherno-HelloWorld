aa4445a68798ff26fff08bc8ec614f507da5eb89
Simon Chen <simonc8192@gmail.com>
2025-05-07 01:16:54 -0700

    print pointer

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+void pp(const int& i)
+{
+	std::cout << &i << " " << i << " " << sizeof(i) << std::endl;
+}
+
 int main()
 {
+	int arr[10] = {};
+	int* harr = new int[10];
 
+	for (size_t i = 0; i < 10; i++)
+	{
+		pp(arr[i]);
+	}
+	for (size_t i = 0; i < 10; i++)
+	{
+		pp(harr[i]);
+	}
+
+	delete[] harr;
 	return 0;
 }
