730fc7e2576e89d1a417246276a36ff1d159cfee
Simon Chen <simonc8192@gmail.com>
2025-05-06 19:23:39 -0700

    testing loops

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
+	std::vector<int> vec;
+	vec.push_back(42);
+	vec.push_back(10);
+	vec.push_back(28);
+	
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << std::endl;
+	}
+	for (auto v : vec)
+	{
+		std::cout << v << std::endl;
+	}
+
+	return 0;
 }
